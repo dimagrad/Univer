94ed5a1a755d37fb9f62da0ef5f268e516ca7bb1
dimagrad <dima.belyj2012@gmail.com>
2021-06-08 17:22:55 +0300

    add code: add students, show students and courses, add student to course

--- src/com/vector/entity/Course.java
+++ src/com/vector/entity/Course.java
@@ -1,26 +1,71 @@
 package com.vector.entity;
 
+import java.util.Date;
+
 public class Course {
-    public Course(String n, int pte, int p) {
+    public Course(String n, int pte, int p, Student t, String d) {
         nazva = n;
         PTE = pte;                //people to enter
         para = p;
+        date = d;
+        newS = t;
+
     }
-    public String getNazva(){
+    public String getName(){
+
         return nazva;
     }
+
+    public String getDate(){
+        return date;
+    }
     public int getPTE(){
+
         return PTE;
     }
     public int getPara(){
+
         return para;
+    }
+    public String getTime(){
+        addTime();
+        return date;
+    }
+
+    public void setStudent(Student t) {
+        this.newS = t;
+        this.PTE--;
+
+    }
+    public Student getStudent(){
+        return this.newS;
+    }
+
+    public void addTime(){
+        switch (para) {
+            case 1:
+                this.date = "8.30 - 10.00";
+                break;
+            case 2:
+                this.date = "10.30 - 12.00";
+                break;
+            case 3:
+                this.date = "12.30 - 14.00";
+                break;
+            case 4:
+                this.date = "14.30 - 16.00";
+                break;
+        }
     }
 
     public void outp(){
-        System.out.println("Name of schedule: "+getNazva()+" people to enter: "+getPTE()+" Position: "+getPara());
-    }
 
+        System.out.println("Name of schedule: " + getName()+" people to enter: " + getPTE()
+                +" Position: " + getPara() + " Time: " + getTime());
+    }
+    private Student newS;
     private String nazva;
     private int PTE;
+    private String date;
     private int para;
 }
--- src/com/vector/entity/Teacher.java
+++ src/com/vector/entity/Teacher.java
@@ -1,20 +1,24 @@
 package com.vector.entity;
 
 public class Teacher {
-    public Teacher(String n, double s){
+    public Teacher(String n, double s, Course c){
         name = n;
         salary = s;
+
     }
 
     public String getName(){
+
         return name;
     }
 
     public double getSalary(){
+
         return salary;
     }
 
     public void outp(){
+
         System.out.println("Name: "+getName()+" Salary = "+getSalary());
     }
 
